201267e6d3a942337c8386f2544b605b77cb0bef
Li Haoyi <haoyi@dropbox.com>
2015-01-09 07:16:11 -0800

    fiddle for deployment

--- shared/Shared.scala
+++ shared/Shared.scala
@@ -24,7 +24,7 @@ object Shared{
 
   val gistId = "9443f8e0ecc68d1058ad"
 
-  val url = "http://www.scala-js-fiddle.com/"
+  val url = "http://www.scala-js-fiddle.com"
 }
 
 class Web extends ClassfileAnnotation
